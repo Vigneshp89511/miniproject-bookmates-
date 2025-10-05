fcb1ebd747c201656efc112fb6c1d39a011f3994
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-10-05 19:05:54 +0530

    Added all component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Home from './pages/Home'
 import Auth from './pages/Auth'
-import Dashboard from './pages/dashboard' 
+import Dashboard from './pages/Dashboard'
 import BookContributorDashboard from './pages/BookContributor'
+import ChooseDashboard from './pages/ChooseDashboard'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Route path='/auth' element={<Auth />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/book-contributor' element={<BookContributorDashboard />} />
+        <Route path='/choose-dashboard' element={<ChooseDashboard />} />
       </Routes>
     </Router>
   )
