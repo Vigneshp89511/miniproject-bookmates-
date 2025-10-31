faf02347ee1c6bcfe0d2e1fd2c0319aed50b3b0f
Vignesh.P <vvigneshp169@gmail.com>
2025-10-31 21:35:13 +0530

    backend auth updated

---
+++ backend/routes/mail.js
@@ -0,0 +1,17 @@
+import nodemailer from 'nodemailer'
+ 
+const transporter = nodemailer.createTransport({
+    host:"smtp.gmail.com",
+    port:587,
+    secure:false,
+    auth:{
+        user:"database189511@gmail.com",
+        pass:"svjdljhxshdknkwg" 
+    },
+});
+
+transporter.verify((error, success) => {
+  console.log(error || success);
+});
+
+export default transporter;
