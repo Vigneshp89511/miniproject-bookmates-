893c6867b06fd21e2b7117d76706af6132ea5894
Vignesh.P <vvigneshp169@gmail.com>
2025-11-03 15:47:59 +0530

    nodemailer updated

--- backend/routes/mail.js
+++ backend/routes/mail.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer'
  
 const transporter = nodemailer.createTransport({
     host:"smtp.gmail.com",
-    port:587,
+    port:587||25||465,
     secure:false,
     auth:{
         user:"database189511@gmail.com",
